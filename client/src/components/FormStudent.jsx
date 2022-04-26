117f51724206c1b998ca7a20a080d2962d8435c8
Yatharth Kale <54075161+yatharth23112001@users.noreply.github.com>
2022-04-26 10:07:13 +0530

    added student registration form

--- client/src/components/FormStudent.jsx
+++ client/src/components/FormStudent.jsx
@@ -1,9 +1,53 @@
 import React from "react";
 
-function FormStudent() {
+function FormStudent(props) {
+
+  let mis="";
+  let email="";
+  
   const on = () => {
     alert("hello");
   }
+
+  const setMis = (_mis) => {
+    mis = _mis;
+  }
+
+  const setEmail = (_email) => {
+    email = _email;
+  }
+
+  const inputMis = (e) =>{
+    var _mis = e.target.value;
+    setMis(_mis);
+  }
+
+  const inputEmail = (e) =>{
+    var _email = e.target.value;
+    setEmail(_email);
+  }
+
+  const signup = async () => {
+    try {
+      await props.Web3States.contractInst.methods.addStudent(mis, email, props.Web3States.accounts[0])
+        .send({ from: props.Web3States.accounts[0] });
+    }
+    catch (error) {
+      alert(error.message);
+      return false;
+    }
+    const state = await props.Web3States.contractInst.methods.getAddStudentState().call();
+    if(state==1) {
+      alert("Student added successfully!");
+    }
+    else if(state==2) {
+      alert("Student already exists!");
+    }
+    else {
+      alert("##########");
+    }
+  }
+
   return (
     <div class="flex mt-8 lg:w-1/2 lg:justify-end lg:mt-0">
       <div class="max-w-sm bg-white rounded-lg dark:bg-gray-800">
@@ -19,12 +63,14 @@ function FormStudent() {
                 type="text"
                 placeholder="MIS Number"
                 aria-label="MIS Number"
+                onChange={inputMis}
               />
               <input
                 class="block w-full px-4 py-2 mt-4 text-gray-300 placeholder-gray-400 bg-white border rounded-md dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-500 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:ring-blue-300 focus:outline-none focus:ring"
                 type="email"
                 placeholder="Email Address"
                 aria-label="Email Address"
+                onChange={inputEmail}
               />
             </div>
 
@@ -35,7 +81,8 @@ function FormStudent() {
               >
                 Already registered?
               </button>
-              <button class="px-4 py-2 font-semibold text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-800 dark:hover:bg-gray-700 focus:outline-none focus:bg-gray-800 dark:focus:bg-gray-700">
+              <button class="px-4 py-2 font-semibold text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-gray-800 dark:hover:bg-gray-700 focus:outline-none focus:bg-gray-800 dark:focus:bg-gray-700"
+                      onClick={signup}>
                 Sign up!
               </button>
             </div>
